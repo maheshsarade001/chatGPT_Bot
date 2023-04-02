32c28b23331baa4c92d98de762e25a0d58ac112d
maheshsarade001 <maheshsarade001@gmail.com>
2023-04-02 18:05:01 +0530

    fix env

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    API: process.env.API,
-    APIKEY: process.env.APIKEY,
-  },
 };
 
 module.exports = nextConfig;
