7bb260fc9a1b92eece5da01e291ef942bd172eb7
maheshsarade001 <maheshsarade001@gmail.com>
2023-04-02 17:45:06 +0530

    complete

---
+++ src/helper/AxiosInstance.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const API = process.env.NEXT_PUBLIC_API || "https://api.openai.com/v1";
+
+const apiKey = process.env.NEXT_PUBLIC_APIKEY;
+
+console.log(apiKey);
+const AxiosInstance = axios.create({
+  baseURL: API,
+  headers: {
+    Authorization: `Bearer ${apiKey}`,
+  },
+});
+
+export default AxiosInstance;
