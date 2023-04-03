a828f6810d2a76ec0fbb5bdf5b961f39d2972f5b
maheshsarade001 <maheshsarade001@gmail.com>
2023-04-04 01:24:50 +0530

    added image search feat

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { Inter } from "next/font/google";
 import ChatGptLogo from "../../public/ChatGPT_logo.png";
 import Image from "next/image";
@@ -44,6 +45,61 @@ export default function Home() {
         let obj = {
           msg,
           fromBot: true,
+        };
+
+        setChats([
+          ...chats,
+          {
+            msg: tempText,
+            fromBot: false,
+          },
+          obj,
+        ]);
+        setLoading(false);
+        localStorage.setItem(
+          "chats",
+          JSON.stringify([
+            ...chats,
+            {
+              msg: tempText,
+              fromBot: false,
+            },
+            obj,
+          ])
+        );
+      })
+      .catch((err) => {
+        console.log(err.response);
+        setLoading(false);
+      });
+  };
+  const onImageSearch = () => {
+    if (!inputText) {
+      return alert("Please enter a message");
+    }
+    const tempText = inputText;
+    setInputText("");
+    setLoading(true);
+    let obj = {
+      msg: tempText,
+      fromBot: false,
+    };
+    setChats([...chats, obj]);
+
+    let data = {
+      prompt: tempText,
+      n: 1,
+      size: "512x512",
+    };
+    AxiosInstance.post("/images/generations", data)
+      .then((res) => {
+        const { data } = res;
+        const msg = data.data[0].url;
+        console.log(msg);
+        let obj = {
+          msg,
+          fromBot: true,
+          isImg: true,
         };
 
         setChats([
@@ -156,9 +212,13 @@ export default function Home() {
                         } `}
                       >
                         <div className="relative w-auto px-4 py-2 text-gray-700 rounded shadow dark:bg-slate-600">
-                          <span className="block dark:text-gray-50">
-                            <ReactMarkdown>{chat.msg}</ReactMarkdown>
-                          </span>
+                          {chat.isImg ? (
+                            <img src={chat.msg} alt="" />
+                          ) : (
+                            <span className="block dark:text-gray-50">
+                              <ReactMarkdown>{chat.msg}</ReactMarkdown>
+                            </span>
+                          )}
                         </div>
                       </li>
                     ))}
@@ -201,6 +261,22 @@ export default function Home() {
                       fill="currentColor"
                     >
                       <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
+                    </svg>
+                  </button>
+                  <button onClick={onImageSearch}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="w-5 h-5 text-gray-500 ml-2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                      />
                     </svg>
                   </button>
                 </div>
